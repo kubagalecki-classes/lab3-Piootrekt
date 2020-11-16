f2e96b1a2221eaf3603d222c818dfc429456e6ec
Piootrekt <ppreform@gmail.com>
2020-11-16 13:30:49 +0000

    zal zrobione

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+using namespace std;
+class Figura
+{
+public:
+    Figura(double pole_) : pole{pole_} {}
+    double       getp() { return pole; }
+    virtual void id() { cout << "To jest Figura o polu " << this->getp() << endl; }
+    // void   setp(double pole_in) { pole = pole_in; }
+
+private:
+    double pole;
+
+protected:
+    // double pole;
+};
+
+class Kolo : public Figura
+{
+public:
+    Kolo(double r) : Figura{3.14159 * r * r} {}
+    void id() override { cout << "To jest Kolo o polu " << this->getp() << endl; }
+};
+
+class Kwadrat : public Figura
+{
+public:
+    Kwadrat(double a) : Figura{a * a} {}
+    void id() override { cout << "To jest Kwadrat o polu " << this->getp() << endl; }
+};
+
+void id(const Figura& f)
+{
+    f->id();
+}
 
 int main()
 {
-    puts("Hello, World!");
+    Kwadrat k1(5);
+    cout << k1.getp() << endl;
+    k1.id();
+    id(k1);
 }
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "Stocznia.hpp"
+#include <typeinfo>
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int towar_trans = 0;
+    unsigned int licz_zagle = 0;
+    while (towar_trans < towar)
+    {
+        Stocznia stocznia{};
+        Statek* s1 = stocznia();
+        towar_trans += s1 -> transportuj();
+        if (typeid(*s1).name() == typeid(class Zaglowiec).name())
+        {
+            licz_zagle++;
+        }
+    }
+    return licz_zagle;
 }
